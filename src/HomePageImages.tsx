82af186d09df259428da4bd2d12c166c7fd6dbfa
joacoague15 <joacoague15@gmail.com>
2023-02-10 23:07:03 +0000

    First version of stable diffusion intro on homepage

--- src/HomePageImages.tsx
+++ src/HomePageImages.tsx
@@ -3,17 +3,26 @@ import React from "react";
 
 const HomePageImages = () => {
     const images = [
-        'https://images.unsplash.com/photo-1537944434965-cf4679d1a598',
-        'https://images.unsplash.com/photo-1538032746644-0212e812a9e7',
-        'https://images.unsplash.com/photo-1537996194471-e657df975ab4'
-    ];
+        {
+            url: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598',
+            phrase: 'A San Francisco Bridge',
+        },
+        {
+            url: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7',
+            phrase: 'A hand and a bird in the snow',
+        },
+        {
+            url: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4',
+            phrase: 'A Chinese temple',
+        }
+    ]
 
         return (
             <Carousel style={{ margin: "auto", marginTop: 20, width: "50%" }} autoplay dotPosition="top">
-                {images.map((image, index) => (
+                {images.map((images, index) => (
                     <div key={index}>
-                        <img style={{ width: 600, height: 600, margin: "auto" }} src={image} alt="slide" />
-                        <h3> HELLO HELLOHEOvHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO</h3>
+                        <img style={{ width: "40%", height: "40%", margin: "auto" }} src={images.url} alt="slide" />
+                        <h3 style={{ marginTop: 5 }}>{images.phrase}</h3>
                     </div>
                 ))}
             </Carousel>
